260a9ae9d54ad342366c3aea09d2035cb60e030e
Shreya-Manepalli <114294836+Shreya-Manepalli@users.noreply.github.com>
2023-02-16 14:22:47 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, I'm PES2UG20CS330\n";
+  cout << "Hello, I'm PES2UG20CS328\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
